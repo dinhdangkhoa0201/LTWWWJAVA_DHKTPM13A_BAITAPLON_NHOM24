b46d7f5cff0a0df4145958ce95fe48b296dd2497
Khoa Cyruss <dinhdangkhoa0201@gmail.com>
2020-06-14 09:31:32 +0700

    file SQL

---
+++ SQLQuery1.sql
@@ -0,0 +1,55 @@
+create database LTWWWJAVA_Group20
+
+
+select * from account
+
+select * from account_role
+
+select * from role
+
+select * from category
+
+select * from verify_account
+
+select * from [dbo].[unit_measure]
+
+select * from product
+
+select * from sale_order_header
+
+select * from sale_order_detail
+
+select * from supplier
+
+select * from category
+
+use LTWWWJAVA_Group20
+
+select * from account where email = 'dinhdangkhoa0201@gmail.com'
+
+delete from sale_order_detail
+
+delete from sale_order_header
+
+delete from product
+
+delete from category
+
+delete from supplier
+
+delete from [dbo].[unit_measure]
+
+delete from account_role
+
+delete from verify_account
+
+delete from account
+
+delete from role
+
+
+use master
+
+
+drop database LTWWWJAVA_Group20
+
